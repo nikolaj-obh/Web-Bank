55d7c5e9ec55038f541fb852d1aa323dfd356aa4
Nikolaj <nikolaj.hansen93@gmail.com>
2023-01-10 05:57:01 +0100

    initial commit

---
+++ src/main/java/dk/nikolaj/webbank/WebBankApplication.java
@@ -0,0 +1,13 @@
+package dk.nikolaj.webbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/nikolaj/webbank/WebBankApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.nikolaj.webbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
