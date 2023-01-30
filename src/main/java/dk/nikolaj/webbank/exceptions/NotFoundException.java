9fe943dc68938e450c1174a1e0c7a53563f51d93
Nikolaj <nikolaj.hansen93@gmail.com>
2023-01-30 21:20:11 +0100

    Fixed login reference

---
+++ src/main/java/dk/nikolaj/webbank/exceptions/NotFoundException.java
@@ -0,0 +1,16 @@
+package dk.nikolaj.webbank.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class NotFoundException extends RuntimeException {
+
+    @ExceptionHandler(value = NotFoundException.class)
+    public ResponseEntity<Object> exception(NotFoundException exception) {
+        return new ResponseEntity<>("Not found: " + exception, HttpStatus.NOT_FOUND);
+    }
+
+}
