0b5fa03f46cea8770c65b1ce799d5806864a661d
Nikolaj <nikolaj.hansen93@gmail.com>
2023-01-30 21:21:14 +0100

    Added NotFoundException class

--- src/main/java/dk/nikolaj/webbank/exceptions/NotFoundException.java
+++ src/main/java/dk/nikolaj/webbank/exceptions/NotFoundException.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class NotFoundException extends RuntimeException {
-
     @ExceptionHandler(value = NotFoundException.class)
     public ResponseEntity<Object> exception(NotFoundException exception) {
         return new ResponseEntity<>("Not found: " + exception, HttpStatus.NOT_FOUND);
