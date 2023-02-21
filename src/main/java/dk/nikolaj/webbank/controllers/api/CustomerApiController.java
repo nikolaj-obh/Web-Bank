0ea4761c5296dccdaf371892944fd54f5f7fef14
Nikolaj <nikolaj.hansen93@gmail.com>
2023-02-21 04:01:54 +0100

    Added few rest api controllers and swagger ui

---
+++ src/main/java/dk/nikolaj/webbank/controllers/api/CustomerApiController.java
@@ -0,0 +1,59 @@
+package dk.nikolaj.webbank.controllers.api;
+
+import dk.nikolaj.webbank.model.Customer;
+import dk.nikolaj.webbank.service.interfaces.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/customer-api")
+public class CustomerApiController {
+
+    CustomerService customerService;
+
+    public CustomerApiController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("")
+    public List<Customer> list() {
+        return customerService.listAllCustomers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> get(@PathVariable Long id) {
+        try {
+            Customer customer = customerService.getCustomer(id);
+            return new ResponseEntity<Customer>(customer, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/")
+    public void add(@RequestBody Customer customer) {
+        customerService.saveCustomer(customer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody Customer customer, @PathVariable Long id) {
+        try {
+            Customer existCustomer = customerService.getCustomer(id);
+            customer.setID(id);
+            customerService.saveCustomer(customer);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+
+        customerService.deleteCustomer(id);
+    }
+}
